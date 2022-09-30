13d8b1e033391db8e20c06dc2baff4340073fdc7
Albert Vega camacho <albertcuentauniversidad@gmail.com>
2022-09-29 19:25:20 -0600

    Version  4++

--- Client.cpp
+++ Client.cpp
@@ -3,9 +3,81 @@
 #include <opencv4/opencv2/opencv.hpp>
 #include <opencv2/core/utils/filesystem.hpp>
 
+#include <opencv2/core.hpp>
+#include <boost/serialization/split_free.hpp>
+#include <boost/serialization/vector.hpp>
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/binary_iarchive.hpp>
+#include <boost/archive/binary_oarchive.hpp>
+
+#include <cassert>
+#include <string>
+#include <vector>
 
 using namespace cv;
 using namespace std;
+/*
+ * serialization
+ */
+BOOST_SERIALIZATION_SPLIT_FREE( cv::Mat )
+
+namespace boost {
+    namespace serialization {
+
+        template <class Archive>
+        void save( Archive & ar, const cv::Mat & m, const unsigned int version )
+        {
+            size_t elemSize = m.elemSize();
+            size_t elemType = m.type();
+
+            ar & m.cols;
+            ar & m.rows;
+            ar & elemSize;
+            ar & elemType;
+
+            const size_t dataSize = m.cols * m.rows * m.elemSize();
+            for ( size_t i = 0; i < dataSize; ++i )
+                ar & m.data[ i ];
+        }
+
+
+        template <class Archive>
+        void load( Archive & ar, cv::Mat& m, const unsigned int version )
+        {
+            int cols, rows;
+            size_t elemSize, elemType;
+
+            ar & cols;
+            ar & rows;
+            ar & elemSize;
+            ar & elemType;
+
+            m.create( rows, cols, static_cast< int >( elemType ) );
+            const size_t dataSize = m.cols * m.rows * elemSize;
+            for (size_t i = 0; i < dataSize; ++i)
+                ar & m.data[ i ];
+        }
+    }
+}
+
+std::string save( const cv::Mat & mat )
+{
+    std::ostringstream oss;
+    boost::archive::text_oarchive toa( oss );
+    toa << mat;
+
+    return oss.str();
+}
+
+void load( cv::Mat & mat, const char * data_str )
+{
+    std::stringstream ss;
+    ss << data_str;
+
+    boost::archive::text_iarchive tia( ss );
+    tia >> mat;
+}
 
 /*
  * image divider
@@ -15,14 +87,14 @@ int divideImage(const cv::Mat& img, const int blockWidth, const int blockHeight,
     // Checking if the image was passed correctly
     if (!img.data || img.empty())
     {
-        std::wcout << "Image Error: Cannot load image to divide." << std::endl;
+        std::cout << "Image Error: Cannot load image to divide." << std::endl;
         return EXIT_FAILURE;
     }
 
     // init image dimensions
     int imgWidth = img.cols;
     int imgHeight = img.rows;
-    std::wcout << "IMAGE SIZE: " << "(" << imgWidth << "," << imgHeight << ")" << std::endl;
+    std::cout << "IMAGE SIZE: " << "(" << imgWidth << "," << imgHeight << ")" << std::endl;
 
     // init block dimensions
     int bwSize;
@@ -37,7 +109,7 @@ int divideImage(const cv::Mat& img, const int blockWidth, const int blockHeight,
         int x0 = 0;
         while (x0 < imgWidth)
         {
-            // compute the block height
+            // compute the block witdh
             bwSize = ((x0 + blockWidth) > imgWidth) * (blockWidth - (x0 + blockWidth - imgWidth)) + ((x0 + blockWidth) <= imgWidth) * blockWidth;
 
             // crop block
@@ -53,20 +125,37 @@ int divideImage(const cv::Mat& img, const int blockWidth, const int blockHeight,
     return EXIT_SUCCESS;
 }
 
+string ReadMessage(boost::asio::ip::tcp::socket & socket) {
+    boost::asio::streambuf buf; // Buffer de entrada de mensajes
+    boost::asio::read_until( socket, buf, "\n"); //  Indica que lea mensaje del socket desde el buffer hasta el delimitador \n
+    string data = boost::asio::buffer_cast<const char*>(buf.data()); // Hace cast del buffer de entrada a un char pointer (caracteres legibles)
+    return data; // Retorna el mensaje recibido
+}
+/**
+ *
+ * Funcion que envia mensaje al cliente
+ *
+ * @param socket
+ * @param message
+ */
+void SendMessage(boost::asio::ip::tcp::socket & socket, string message) {
+    string msg = message + "\n"; // Declara variable string con un delimitador linea siguiente
+    boost::asio::write( socket, boost::asio::buffer(msg)); // Envia mensaje a cliente mediante buffer
+}
+
 int main() {
     /*
      * Image management
      */
     cv::Mat image = imread("/home/albert/Documentos/GitHub/Proyecto_I_AyEDII/Prueba.jpeg", IMREAD_COLOR);
-
     if (image.empty()) { //Verify if the image has been readed correctly
         cout << "Image File "
              << "Not Found" << endl;
         cin.get(); // wait for any key press
         return -1;
     }
-    imshow("ImageWindow", image);
-    waitKey(0);
+    //imshow("ImageWindow", image);
+    //waitKey(0);
 
     /*
      * Divide the image
@@ -76,15 +165,15 @@ int main() {
     const int blockh = 128;
     std::vector<cv::Mat> blocks;
     int divideStatus = divideImage(image, blockw, blockh, blocks);
-
     // debug: save blocks
+    /*
     cv::utils::fs::createDirectory("blocksFolder");
     for (int j = 0; j < blocks.size(); j++)
     {
         std::string blockId = std::to_string(j);
         std::string blockImgName = "blocksFolder/block#" + blockId + ".jpeg";
         imwrite(blockImgName, blocks[j]);
-    }
+    }*/
 
     /*
      * Socket
@@ -92,30 +181,23 @@ int main() {
     boost::asio::io_service io_service; // Servicio de input/output
     boost::asio::ip::tcp::socket socket(io_service); // Declaracion de socket para conexiones
     boost::system::error_code error; // Variable para codigo de error especifico de Boost
-    boost::asio::streambuf receive_buffer; // Buffer para recibir mensajes
-    const string outMessage = "Hola, soy el cliente\n"; // Mensaje a enviar
 
     socket.connect(boost::asio::ip::tcp::endpoint( boost::asio::ip::address::from_string("127.0.0.1"), 1234));
     cout << "Conectado al servidor" << endl;
-    boost::asio::write(socket, boost::asio::buffer(outMessage), error );
+    string size = to_string(blocks.size());
 
-    // Escribe mensaje al servidor
+    SendMessage(socket, size);
+    string receivedMessage = ReadMessage(socket);
+    receivedMessage.pop_back();
+    cout << "Server dice que: "<<receivedMessage<<endl;
 
-    if(!error) {
-        cout << "Mensaje enviado" << endl;
-    }
-    else {
-        cout << "error" << error.message() << endl;
-    }
-
-    boost::asio::read(socket, receive_buffer, boost::asio::transfer_all(), error); // Guarda mensaje recibido en el buffer
-
-    if (error && error != boost::asio::error::eof) {
-        cout << "error" << error.message() << endl;
-    } else {
-        const char* inMessage = boost::asio::buffer_cast<const char*>(receive_buffer.data()); // Interpreta mensaje recibido
-        cout << "Servidor dice: " + (string) inMessage << endl; // Se hace cast a string al inMessage ya que es un const char pointer,
-                                                                  // para poder concatenarlo basicamente
+    for (int i = 0; i < blocks.size(); i++){
+        cv::Mat TEMP = blocks[i];
+        std::string serialized = save(TEMP);
+        SendMessage(socket, serialized); // Escribe mensaje al servidor
+        string receivedStatus = ReadMessage(socket);
+        receivedMessage.pop_back();
+        cout << "Server dice: "<<receivedStatus<<endl;
     }
     return 0;
 }
--- Server.cpp
+++ Server.cpp
@@ -1,9 +1,88 @@
 #include <iostream>
+#include <fstream>
 #include <boost/asio.hpp>
-#include <opencv4/opencv2/opencv.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/utils/filesystem.hpp>
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/stitching.hpp"
 
+
+
+#include <opencv2/core.hpp>
+#include <boost/serialization/split_free.hpp>
+
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/archive/text_oarchive.hpp>
+
+
+
+#include <string>
+#include <vector>
 using namespace std;
 using namespace cv;
+
+/*
+ * serialization
+ */
+BOOST_SERIALIZATION_SPLIT_FREE( cv::Mat )
+
+namespace boost {
+    namespace serialization {
+
+        template <class Archive>
+        void save( Archive & ar, const cv::Mat & m, const unsigned int version )
+        {
+            size_t elemSize = m.elemSize();
+            size_t elemType = m.type();
+
+            ar & m.cols;
+            ar & m.rows;
+            ar & elemSize;
+            ar & elemType;
+
+            const size_t dataSize = m.cols * m.rows * m.elemSize();
+            for ( size_t i = 0; i < dataSize; ++i )
+                ar & m.data[ i ];
+        }
+
+
+        template <class Archive>
+        void load( Archive & ar, cv::Mat& m, const unsigned int version )
+        {
+            int cols, rows;
+            size_t elemSize, elemType;
+
+            ar & cols;
+            ar & rows;
+            ar & elemSize;
+            ar & elemType;
+
+            m.create( rows, cols, static_cast< int >( elemType ) );
+            const size_t dataSize = m.cols * m.rows * elemSize;
+            for (size_t i = 0; i < dataSize; ++i)
+                ar & m.data[ i ];
+        }
+
+    } // namespace serialization
+} // namespace boost
+
+std::string save( const cv::Mat & mat )
+{
+    std::ostringstream oss;
+    boost::archive::text_oarchive toa( oss );
+    toa << mat;
+
+    return oss.str();
+}
+
+void load( cv::Mat & mat, const char * data_str )
+{
+    std::stringstream ss;
+    ss << data_str;
+
+    boost::archive::text_iarchive tia( ss );
+    tia >> mat;
+}
 
 /**
  *
@@ -25,27 +104,51 @@ string ReadMessage(boost::asio::ip::tcp::socket & socket) {
  * @param socket
  * @param message
  */
-void SendMessage(boost::asio::ip::tcp::socket & socket, const string& message) {
-    const string msg = message + "\n"; // Declara variable string con un delimitador linea siguiente
-    boost::asio::write( socket, boost::asio::buffer(message)); // Envia mensaje a cliente mediante buffer
+void SendMessage(boost::asio::ip::tcp::socket & socket, string message) {
+    string msg = message + "\n"; // Declara variable string con un delimitador linea siguiente
+    boost::asio::write( socket, boost::asio::buffer(msg)); // Envia mensaje a cliente mediante buffer
 }
 
 int main() {
     boost::asio::io_service io_service; // Servicio de input/output
-    boost::asio::ip::tcp::acceptor acceptor_(io_service, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), 1234));
-                                            // Acepta de manera asincrona conexiones en puerto 1234
+    boost::asio::ip::tcp::acceptor acceptor_(io_service, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(),
+                                                                                        1234));// Acepta de manera asincrona conexiones en puerto 1234
     boost::asio::ip::tcp::socket socket_(io_service); // Declaracion de socket para conexiones
+    boost::system::error_code error;
 
     cout << "Servidor iniciado" << endl;
-
     acceptor_.accept(socket_); // Acepta al socket del cliente que pida conectarse
-
     cout << "Cliente conectado" << endl;
 
-    string message = ReadMessage(socket_); // Lee y declara mensaje del cliente
-    cout << "Cliente dice: " + message << endl;
+    string sizeMessage = ReadMessage(socket_);
+    sizeMessage.pop_back(); // Popping last character "\n"
+    SendMessage(socket_, "Tamaño recibido");
+    int size = stoi(sizeMessage);
 
-    SendMessage(socket_, "Hola, soy el servidor"); // Envia mensaje al cliente
-    cout << "Mensaje enviado" << endl;
+    vector<cv::Mat> blocks;
+
+    for (int i = 0; i < size; i++) {
+        string message = ReadMessage(socket_); // Lee y declara mensaje del cliente
+        SendMessage(socket_, "Pedazo " + to_string(i) + " recibido");
+        message.pop_back();
+        Mat result;
+        load(result, message.c_str());
+        //imshow("prueba", result);
+        //waitKey(0);
+        blocks.push_back(result);
+    }
+
+    /*
+     * REBUILD
+     */
+
+    cv::utils::fs::createDirectory("Result");
+    for (int j = 0; j < blocks.size(); j++)
+    {
+        std::string blockId = std::to_string(j);
+        std::string blockImgName = "Result/block#" + blockId + ".jpeg";
+        imwrite(blockImgName, blocks[j]);
+    }
+
     return 0;
 }
